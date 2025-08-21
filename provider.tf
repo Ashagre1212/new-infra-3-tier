b15dab4303482ae0442327aebe4acfe638aeb0e0
Alemayehu1234 <100979280+Alemayehu1234@users.noreply.github.com>
2025-08-21 08:35:39 -0400

    Update provider.tf
    change on provider block

--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.59.0"
+      version = "~> 6.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
 
