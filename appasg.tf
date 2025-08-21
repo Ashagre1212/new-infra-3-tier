48c54748fa85baa275ba663db5ad923cb2c757f2
Alemayehu1234 <100979280+Alemayehu1234@users.noreply.github.com>
2025-08-21 05:28:41 -0400

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -21,7 +21,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0861f4e788f5069dd"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "3tierproject"
 
   network_interfaces {
