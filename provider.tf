4aee7530ba8178281225338032d2212bf772e0b9
Alemayehu1234 <100979280+Alemayehu1234@users.noreply.github.com>
2025-08-21 05:31:18 -0400

    Update provider.tf
    alex region update

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "6.9.0"
     }
   }
 }
