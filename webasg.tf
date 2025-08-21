6a77ff4e429dc144bb76e8ea1a25cba741c33df5
Alemayehu1234 <100979280+Alemayehu1234@users.noreply.github.com>
2025-08-21 07:51:35 -0400

    Update webasg.tf
    change-name

--- webasg.tf
+++ webasg.tf
@@ -16,7 +16,7 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-00ca32bbc84273381"
   instance_type = "t3.micro"
-  key_name      = "3tierproject"
+  key_name      = "terraform-3-tier"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
