abff184929b81f2b4cf0ac3dee31bfb0cf843deb
Ashagre1212 <alem189@outlook.com>
2025-08-22 14:09:34 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,18 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
+/*resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform-fordemoinfra3-01"
 
   # Enable versioning
   versioning {
     enabled = true
+  }
+} */
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = "aws_s3_terraform_3teir_bucket"
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
