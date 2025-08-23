4badc7cbe1a5be8ba75fab1ff6bb85d873ce304a
Ashagre1212 <alem189@outlook.com>
2025-08-23 03:39:21 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor4pmbatch-01"
+  bucket = "swiggy-bucket-terraform-demoinfra3tirerrrrfor4pmbatch-01"
 }
 
